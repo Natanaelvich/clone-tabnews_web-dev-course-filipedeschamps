5f3262e71bb47a82706823daa861ba4342595cc7
natanaelvich <taelima1997@gmail.com>
2023-08-17 22:39:18 -0300

    add config jest alias

--- jest.config.js
+++ jest.config.js
@@ -25,7 +25,7 @@ module.exports = {
       '^.+\\.(png|jpg|jpeg|gif|webp|avif|ico|bmp|svg)$/i': `<rootDir>/__mocks__/fileMock.js`,
    
       // Handle module aliases
-      '^@/components/(.*)$': '<rootDir>/components/$1',
+      '^@/(.*)$': '<rootDir>/src/$1',
     },
     // Add more setup options before each test is run
     // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
