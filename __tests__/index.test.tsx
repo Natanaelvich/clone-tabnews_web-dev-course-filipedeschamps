7a7a8cdc9daace786668160fd3473b2d6b0ca49c
natanaelvich <taelima1997@gmail.com>
2023-08-17 22:02:29 -0300

    add initial test

---
+++ __tests__/index.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react'
+import Home from '../src/app/page'
+ 
+describe('Home', () => {
+  it('renders a heading', () => {
+    render(<Home />)
+ 
+    const heading = screen.getByRole('heading', {
+      name: /Tabnews Clone/i,
+    })
+ 
+    expect(heading).toBeInTheDocument()
+  })
+})
