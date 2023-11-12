b4d7f28e5fe2be59fd5077d8c77fdd294227e4f3
natanaelvich <taelima1997@gmail.com>
2023-11-12 20:42:23 -0300

    Add PostgreSQL database connection and query function

---
+++ infra/database.ts
@@ -0,0 +1,22 @@
+import { Client } from "pg";
+
+async function query(queryObject: any) {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: parseInt(process.env.POSTGRES_PORT as string),
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+  await client.connect();
+  const result = await client.query(queryObject);
+  await client.end();
+  return result;
+  return result;
+}
+
+const database = {
+  query: query,
+};
+
+export default database;
