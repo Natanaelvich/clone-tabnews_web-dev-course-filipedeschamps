353afbe80e8d3032c7c0f02ca0e8ce6b495df7cb
natanaelvich <taelima1997@gmail.com>
2024-02-10 15:16:50 -0300

    Add database status endpoint implementation

--- src/app/api/v1/status/route.ts
+++ src/app/api/v1/status/route.ts
@@ -1,8 +1,34 @@
 import database from "@infra/database";
+import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  const result = await database.query("SELECT 1 + 1 as sum");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
 
-  return Response.json({ status: "ok" });
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;"
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  return NextResponse.json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
