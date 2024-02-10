40d4d22a85989a9e20900796aa9457c5117ae746
natanaelvich <taelima1997@gmail.com>
2024-02-10 15:16:54 -0300

    Refactor database.ts to use environment variables and handle connection errors

--- infra/database.ts
+++ infra/database.ts
@@ -1,21 +1,40 @@
+import { env } from "@/utils/env";
 import { Client } from "pg";
 
-async function query(queryObject: any) {
+async function query(queryObject: string | { text: string; values: any[] }) {
   const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: parseInt(process.env.POSTGRES_PORT as string),
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
+    host: env.POSTGRES_HOST,
+    port: Number(env.POSTGRES_PORT),
+    user: env.POSTGRES_USER,
+    database: env.POSTGRES_DB,
+    password: env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
   });
-  await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
-  return result;
+
+  try {
+    await client.connect();
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    await client.end();
+  }
 }
 
 const database = {
-  query: query,
+    query: query,
 };
 
 export default database;
+
+function getSSLValues() {
+    if (process.env.POSTGRES_CA) {
+        return {
+            ca: process.env.POSTGRES_CA,
+        };
+    }
+
+    return process.env.NODE_ENV === "development" ? false : true;
+}
