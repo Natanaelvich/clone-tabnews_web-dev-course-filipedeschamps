af29bd57febf812e0e2b09d54beec8075c4b9514
natanaelvich <taelima1997@gmail.com>
2024-07-18 21:30:14 -0300

    chore: Refactor test for POST /api/v1/migrations endpoint

--- tests/integration/api/v1/migrations/post.test.ts
+++ tests/integration/api/v1/migrations/post.test.ts
@@ -3,7 +3,9 @@ import database from "infra/database.js";
 beforeAll(cleanDatabase);
 
 async function cleanDatabase() {
-  await database.query("drop schema public cascade; create schema public;");
+  await database.query({
+    text: "drop schema public cascade; create schema public;",
+  });
 }
 
 test("POST to /api/v1/migrations should return 200", async () => {
