b739eba1d59a9bf8676b4f77eeaab29137f8fd6f
natanaelvich <taelima1997@gmail.com>
2024-07-18 21:29:55 -0300

    chore: Refactor test for GET /api/v1/migrations endpoint

--- tests/integration/api/v1/migrations/get.test.ts
+++ tests/integration/api/v1/migrations/get.test.ts
@@ -1,9 +1,13 @@
 import database from "infra/database.js";
+import orchestrator from "../../../../orchestrator";
 
 beforeAll(cleanDatabase);
 
 async function cleanDatabase() {
-  await database.query("drop schema public cascade; create schema public;");
+  await orchestrator.waitForAllServices();
+  await database.query({
+    text: "drop schema public cascade; create schema public;",
+  });
 }
 
 test("GET to /api/v1/migrations should return 200", async () => {
